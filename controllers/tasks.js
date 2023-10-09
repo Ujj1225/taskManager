c1d49d65ea1d99da4304c06f2302c03e31c2b1af
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-09 19:09:15 +0545

    Updated to work on delete and get methods

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,31 +2,65 @@
 // Instance of a model is document
 const Task = require("../models/task");
 
-const getAllTasks = (req, res) => {
-  res.send("Fetching all items");
+const getAllTasks = async (req, res) => {
+  try {
+    // .find({}) gets all the documents of collection
+    const task = await Task.find({});
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({
+      message: "could not fetch all tasks",
+    });
+  }
 };
 
 const postTasks = async (req, res) => {
-  const {name, completed} = req.body;
-  const task = await Task.create({
-    name, 
-    completed
-  });
-  res.status(201).send(task);
+  try {
+    const { name, completed } = req.body;
+    const task = await Task.create({
+      name,
+      completed,
+    });
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({
+      msg: error,
+    });
+  }
 };
 
-const getOneTask = (req, res) => {
-  res.json({
-    id: req.params.id,
-  });
+const getOneTask = async (req, res) => {
+  try {
+    // The below statement provides the id alias as taskId
+    const { id: taskId } = req.params;
+    // We always check for _id becuase thats what mongo provides as default
+    const spTask = await Task.findOne({ _id: taskId });
+    res.status(200).json({ spTask });
+  } catch (error) {
+    res.status(404).json({
+      attempt: "Successful!",
+      message: "NOT A VALID ID",
+    });
+  }
 };
 
 const updateTasks = (req, res) => {
   res.send(`The task you are trying to update is ${req.params.id}`);
 };
 
-const deleteTasks = (req, res) => {
-  res.send(`The task you are trying to delete is ${req.params.id}`);
+const deleteTasks = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const spTask = await Task.findOneAndDelete({ _id: taskId });
+    if (!spTask) {
+      return res.status(404).json({
+        msg: `No task with id: ${taskId}`,
+      });
+    }
+    res.status(200).json({spTask});
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 module.exports = {
   getAllTasks,
